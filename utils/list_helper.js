1d0691cad12de3089fdb725e8c447eff2ce0a0b2
Manuel Hortelano Rodríguez <manuel.hortelano.rodriguez@gmail.com>
2025-02-14 21:54:54 +0100

    exercise 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -23,8 +23,25 @@ const favoriteBlog = (blogs) => {
   return treatedMatch
 }
 
+const mostBlogs = (blogs) => {
+  const _ = require('lodash')
+
+  const authorsArray = blogs.map((blog) => blog.author)
+  const authorCounts = _.countBy(authorsArray, authorsArray.author)
+  console.log('!!!!!', authorCounts)
+  const max = Math.max(...Object.values(authorCounts))
+  getKeyByValue = (object, value) => {
+    return Object.keys(object).find((key) => object[key] === value)
+  }
+  return {
+    author: getKeyByValue(authorCounts, max),
+    blogs: max,
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
