87636a622b0287219c2736cf17268efeef361b2a
Manuel Hortelano Rodríguez <manuel.hortelano.rodriguez@gmail.com>
2025-02-14 18:44:19 +0100

    exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return !blogs
+    ? 0
+    : blogs.length === 1
+    ? blogs[0].likes
+    : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
