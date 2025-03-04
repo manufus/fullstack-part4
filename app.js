ae52d9a5110c5952f81d0d747223e2f230670cca
Manuel Hortelano Rodríguez <manuel.hortelano.rodriguez@gmail.com>
2025-03-04 18:38:36 +0100

    exercise 4.18

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const notesRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
@@ -27,6 +28,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', notesRouter)
 app.use('/api/users', usersRouter)
 
